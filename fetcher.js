f846a3cbc886120758e8079f60005ceb5fc867ef
vagrant <bahar.hosseini@gmail.com>
2022-08-04 04:05:49 +0000

    checking if the file path already exists

--- fetcher.js
+++ fetcher.js
@@ -2,14 +2,32 @@
 const request = require('request');
 const fs = require('fs');
 
-let arg = process.argv.slice(2);
-
-request(arg[0], (error, response, body) => {
- 
-  fs.writeFile(arg[1] , body, err => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(`Downloaded and saved ${body.length} bytes to ${arg[1]}`);
-  });
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+let arg = process.argv.slice(2);
+try {
+  if (fs.existsSync(arg[1])) {
+    readline.question(`one file exists at ${arg[1]} Do you want to overwrite the file(Y)`, ans => {
+      console.log(` ${ans}!`);
+      if (ans === 'Y') {
+        request(arg[0], (error, response, body) => {
+ 
+          fs.writeFile(arg[1], body,err => {
+            if (err) {
+              console.error(err);
+            }
+            console.log(`Downloaded and saved ${body.length} bytes to ${arg[1]}`);
+          });
+        });
+      }
+      readline.close();
+    });
+  }
+
+} catch (err) {
+  console.error(err);
+}
+
